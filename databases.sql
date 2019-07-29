1500769c6ea9f899f39fa24ea99fa397588d38b7
Fernando Faria <fernandordefaria@gmail.com>
2019-07-29 11:55:18 -0300

    Update databases.sql

--- databases.sql
+++ databases.sql
@@ -1,24 +1,82 @@
+--Cria o banco de dados estoque
+create database estoque;
+
 --Cria a tabela de usuários
 drop table users;
 create table users(
-	id int auto_increment primery key,
-	nome varchar(25) not null UNIQUE,
-	senha nome varchar(25) not null,
-	tipo char(1)
+	id serial primary key,
+	name varchar(25) not null UNIQUE,
+	passwd varchar(25) not null,
+	isRoot boolean default false
 );
 --Insere um usuário de testes
-insert into users(nome, senha, tipo)values('teste', 'teste', 'admin');
+insert into users(name, passwd, isRoot)values('teste', 'teste', true);
+insert into users(name, passwd, isRoot)values('test', 'teste', true);
 
+select * from users;
+
+--Tabela de endereços
+drop table addresses;
+create table addresses(
+	id serial primary key,
+	name varchar(30) not null,
+	number int not null,
+	country varchar(10) not null default 'BR',
+	state varchar(2) not null default 'PR'
+);
+--Tabelas de telefone
+drop table phones;
+create table phones(
+	id serial primary key,
+	number varchar(15) not null,
+	status bool default false
+);
 --Cria A tabela de clientes
-drop table cleintes;
-create table clientes(
-	id int auto_increment primery key,
-	nome varchar(25) not null,
-	sobrenome  varchar(25),
-	cpd  varchar(25) not null,
-	sexo char(1),
-	endereco  varchar(25) not null,
-	data_nasc date,
-	telefone varchar(25) not null,
-	email  varchar(25)
+drop table customers;
+create table customers(
+	id serial primary key,
+	name varchar(25) not null,
+	fullName  varchar(25),
+	doc  varchar(25) not null,
+	gender char(1),
+	address int,
+	joiner date default now(),
+	phones int,
+	email  varchar(25),
+	foreign key(address) references addresses(id),
+	foreign key(phones) references phones(id)
+);
+--Tabela de grupo de usuários
+drop table usergroup;
+create table usergroup(
+	id serial primary key,
+	name varchar(25) not null,
+	permission varchar(25) not null,
+	status bool default false
+);
+--Tabela que relaciona os usuários ao grupo
+drop table userusergroup;
+create table userusergroup(
+	users int,
+	usergroup int,
+	foreign key(users) references users(id),
+	foreign key(usergroup) references usergroup(id)
+);
+--Tabela de módulos
+drop table modules;
+create table modules(
+	id serial primary key,
+	name varchar(30) not null,
+	descript varchar(30) not null,
+	icon varchar(30) not null,
+	position int default 0,
+	status boolean default false
+);
+--Tabela de Grupo de Módulos
+drop table modulesgroup;
+create table modulesgroup(
+	modules int,
+	usergroup int,
+	foreign key(modules) references modules(id)
+	foreign key(usergroup) references usergroup(id)
 );
